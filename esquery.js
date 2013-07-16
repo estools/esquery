f755d2e252eec4833b74e7e9a91b9af8633fe26b
Joel Feenstra <jrfeenst@gmail.com>
2013-07-15 21:56:12 -0400

    Add support for sibling, adjacent, child, nth-child, and attribute existence ([attr]) selectors.

--- esquery.js
+++ esquery.js
@@ -1,278 +1,461 @@
 (function () {
 
+	function esqueryModule() {
 
-	function parse(selector) {
-		return processTokens(tokenize(selector));
-	}
+		/**
+		 * Tokenize a selector string into an array of tokens. Tokens
+		 * contain a type and value field.
+		 */
+		function tokenize(selector) {
+	    	var tokens = selector.split(/(:)|\s*(\*|~|>|<=|>=|<|=|\+|\[|\]|\(|\)|\s)\s*/);
 
-	function tokenize(selector) {
-    	var tokens = selector.split(/\s*?(\s|\*|~|<=|>=|<|>|=|:|\+|\[|\]|\(|\))/);
+	    	tokens = tokens.filter(function (token) {
+	    		return token;
+	    	});
 
-    	tokens = tokens.filter(function (token) {
-    		return token;
-    	});
+	    	tokens = tokens.map(function (token) {
+	    		if (token === "*") {
+	    			return {
+	    				type: "wildcard",
+	    				value: "*"
+	    			};
+	    		} else if (/first\-child|nth\-child|last\-child|calc|length/.test(token)) {
+	    			return {
+	    				type: "keyword",
+	    				value: token
+	    			};
+	    		} else if (/".*"/.test(token)) {
+	    			return {
+	    				type: "string",
+	    				value: token.replace(/^"|"$/g, "")
+	    			};
+	    		} else if (/[+\-]?[0-9]*.?[0-9]+/.test(token)) {
+	    			return {
+	    				type: "number",
+	    				value: parseFloat(token)
+	    			};
+	    		} else if (/~|<=|>=|<|>|=|:|\+|\[|\]|\(|\)|\s/.test(token)) {
+	    			return {
+	    				type: "operator",
+	    				value: token
+	    			};
+	    		} else {
+	    			return {
+	    				type: "identifier",
+	    				value: token
+	    			};
+	    		}
+	    	});
 
-    	tokens = tokens.map(function (token) {
-    		if (token === "*") {
-    			return {
-    				type: "wildcard"
-    			};
-    		} else if (/first\-child|nth\-child|last\-child|calc|length/.test(token)) {
-    			return {
-    				type: "keyword",
-    				value: token
-    			};
-    		} else if (/".*"/.test(token)) {
-    			return {
-    				type: "string",
-    				value: token.replace(/^"|"$/g, "")
-    			};
-    		} else if (/[+\-]?[0-9]*.?[0-9]+/.test(token)) {
-    			return {
-    				type: "number",
-    				value: parseFloat(token)
-    			};
-    		} else if (/~|<=|>=|<|>|=|:|\+|\[|\]|\(|\)|\s/.test(token)) {
-    			return {
-    				type: "operator",
-    				value: token
-    			};
-    		} else {
-    			return {
-    				type: "identifier",
-    				value: token
-    			};
-    		}
-    	});
+	    	return tokens;
+		}
 
-    	return tokens;
-	}
+		/**
+		 * Loop through all the tokens and process them into the
+		 * selector AST. The selector AST is a tree containing
+		 * nodes representing the various elements of the selector:
+		 * * descendant
+		 * * child
+		 * * sibling
+		 * * adjacent
+		 * * 'and'
+		 * * nth-child(num)
+		 * * [attribute]
+		 */
+		function processTokens(tokens) {
+			var ast;
+			while (tokens.length > 0) {
+				ast = consumeSelector(tokens, ast);
+			}
+			return ast;
+		}
 
-	function findToken(tokens, token, i) {
-		for (; i < tokens.length; i++) {
-			if (tokens[i].type === token.type && tokens[i].value === token.value) {
-				break;
+		var operatorMap = {
+			" ": "descendant",
+			">": "child",
+			"~": "sibling",
+			"+": "adjacent"
+		};
+
+		/**
+		 * Core token processor
+		 */
+		function consumeSelector(tokens, ast) {
+			var token = tokens.shift();
+			if (token.type === "wildcard" && !ast) {
+				return token;
+			} else if (token.type === "identifier" && !ast) {
+				return token;
+			} else if (token.type === "operator" && tokens.length > 0) {
+				if (/[\s>~+]/.test(token.value)) {
+					var selector = consumeSelector(tokens);
+					return ast ? {
+						type: operatorMap[token.value],
+						left: ast,
+						right: selector
+					} : selector;
+				} else if (token.value === ":") {
+					var pseudo = consumePseudo(tokens);
+					return ast ? {
+						type: "and",
+						left: ast,
+						right: pseudo
+					} : pseudo;
+				} else if (token.value === "[") {
+					var attribute = consumeAttribute(tokens);
+					return ast ? {
+						type: "and",
+						left: ast,
+						right: attribute
+					} : attribute;
+				}
+			} else {
+				throw createError("Unexpected token: ", token, tokens, ast);
 			}
 		}
-		return i;
-	}
 
-	function processTokens(tokens) {
-		var ast;
-		while (tokens.length > 0) {
-			ast = parseSelector(tokens, ast);
-		}
-		return ast;
-	}
-
-	var operatorMap = {
-		" ": "descendant",
-		">": "child",
-		"~": "sibling",
-		"+": "adjacent"
-	}
-
-	function parseSelector(tokens, ast) {
-		var token = tokens.shift();
-		if (token.type === "wildcard" && !ast) {
-			return token;
-		} else if (token.type === "identifier" && !ast) {
-			return token;
-		} else if (token.type === "operator" && tokens.length > 0) {
-			if (/[\s>~+]/.test(token.value)) {
-				return {
-					type: operatorMap[token.value],
-					left: ast,
-					right: parseSelector(tokens)
-				};
-			} else if (token.value === ":") {
-				var pseudo = parsePseudo(tokens, ast);
-				return ast ? {
-					type: "and",
-					left: ast,
-					right: pseudo
-				} : pseudo;
-			} else if (token.value === "[") {
-				var attribute = parseAttribute(tokens, ast);
-				return ast ? {
-					type: "and",
-					left: ast,
-					right: attribute
-				} : attribute;
+		/**
+		 * Consume the various types of pseudo selectors (:*-child).
+		 */
+		function consumePseudo(tokens, ast) {
+			var token = tokens.shift();
+			if (token.type === "keyword") {
+				switch (token.value) {
+				case "first-child":
+					return {
+						type: "nth-child",
+						index: {
+							type: "literal",
+							value: 0
+						}
+					};
+				case "nth-child":
+					return {
+						type: "nth-child",
+						index: consumeArgs(tokens, ast)
+					};
+				case "last-child":
+					return {
+						type: "nth-child",
+						index: {
+							type: "literal",
+							value: -1
+						}
+					};
+				default:
+					throw createError("Unexpected keyword: ", token, tokens, ast);
+				}
+			} else {
+				throw createError("Unexpected token in pseudo: ", token, tokens, ast);
 			}
-		} else {
-			throw createError("Unexpected token: ", token, tokens, ast);
 		}
-	}
 
-	function parsePseudo(tokens, ast) {
-		var token = tokens.shift();
-		if (token.type === "keyword") {
-			switch (token.value) {
-			case "first-child":
-				return {type: "nth-child", index: 0};
-			case "nth-child":
-				return {
-					type: "nth-child",
-					index: parseArgs(tokens, ast)
-				};
-			case "last-child":
-				return {
-					type: "nth-child",
-					index: -1
-				};
-			default:
-				throw createError("Unexpected keyword: ", token, tokens, ast);
-			}
-		} else {
-			throw createError("Unexpected token in pseudo: ", token, tokens, ast);
-		}
-	}
+		/**
+		 * Consume an attribute selector ([])
+		 */
+		function consumeAttribute(tokens, ast) {
+			var token = tokens.shift();
+			if (token.type === "identifier" && tokens.length > 0) {
+				var op = tokens.shift();
+				if (op.type === "operator") {						
+					switch (op.value) {
+					case "=":
+						ast = {
+							type: "attribute",
+							name: token.value,
+							operator: op.value,
+							value: consumeValue(tokens, ast)
+						};
 
-	function parseAttribute(tokens, ast) {
-		var token = tokens.shift();
-		if (token.type === "identifier" && tokens.length > 1 && tokens[0].type === "operator") {
-			ast = {
-				type: "attribute",
-				name: token.value,
-				operator: tokens.shift().value,
-				value: parseValue(tokens, ast)
-			};
+						token = tokens.shift();
+						if (token.type !== "operator" || token.value !== "]") {
+							throw createError("Unexpected token in attribute: ", token, tokens, ast);
+						}
 
-			token = tokens.shift();
-			if (token.type !== "operator" || token.value !== "]") {
+						return ast;
+
+					case "]":
+						return {
+							type: "attribute",
+							name: token.value
+						};
+					default:
+						createError("Unexpected operator in attribute: ", op, tokens, ast);
+					}
+				} else {
+					throw createError("Unexpected token in attribute: ", op, tokens, ast);
+				}
+			} else {
 				throw createError("Unexpected token in attribute: ", token, tokens, ast);
 			}
-
-			return ast;
-		} else {
-			throw createError("Unexpected token in attribute: ", token, tokens, ast);
 		}
-	}
 
-	function parseArgs(tokens, ast) {
-		var token = tokens.shift();
-		if (token.value === "(" && tokens.length > 1) {
-			var literal = parseValue(tokens, ast);
+		/**
+		 * Consume operator argumetns inside parens
+		 */
+		function consumeArgs(tokens, ast) {
+			var token = tokens.shift();
+			if (token.value === "(" && tokens.length > 1) {
+				var literal = consumeValue(tokens, ast);
 
-			var paren = tokens.shift();
-			if (paren.type !== "operator" || paren.value !== ")") {
-				throw createError("Unexpected token in value: ", paren, tokens, ast);
-			}
+				var paren = tokens.shift();
+				if (paren.type !== "operator" || paren.value !== ")") {
+					throw createError("Unexpected token in value: ", paren, tokens, ast);
+				}
 
-			return literal;
-		} else {
-			throw createError("Unexpected token in args: ", token, tokens, ast);
-		}
-	}
-
-	function parseValue(tokens, ast) {
-		var token = tokens.shift();
-		if (token.type === "number" || token.type === "string") {
-			return {
-				type: "literal",
-				value: token.value
-			};
-		} else {
-			throw createError("Unexpected token for value: ", token, tokens, ast);
-		}
-	}
-
-	function createError(message, token, tokens, ast) {
-		return new Error(message + JSON.stringify(token) + ", " + JSON.stringify(tokens) +
-				", " + JSON.stringify(ast));
-	}
-
-	function visitPre(ast, fn, context) {
-		var newContext = fn(ast, context);
-		context = context || newContext;
-
-		var key;
-		for (key in ast) {
-			if (ast[key] && ast[key].forEach) {
-				ast[key].forEach(function (node) {
-					visitPre(node, fn, context);
-				});
-			} else if (ast[key] && ast[key].type) {
-				visitPre(ast[key], fn, context);
-			}
-		}
-	}
-
-	function getPath(obj, key) {
-		var keys = key.split(".");
-		var i;
-		value = obj;
-		for (i = 0; i < keys.length; i++) {
-			if (value[keys[i]] !== undefined) {
-				value = value[keys[i]];
+				return literal;
 			} else {
-				return undefined;
+				throw createError("Unexpected token in args: ", token, tokens, ast);
 			}
 		}
-		return value;
-	}
 
-	function match(ast, selector) {
-		var leftMatches, rightMatches, matches = [];
+		/**
+		 * Consume values (literals and computed values)
+		 */
+		function consumeValue(tokens, ast) {
+			var token = tokens.shift();
+			if (token.type === "number" || token.type === "string") {
+				return {
+					type: "literal",
+					value: token.value
+				};
+			} else {
+				throw createError("Unexpected token for value: ", token, tokens, ast);
+			}
+		}
 
-		switch (selector.type) {
-		case "wildcard":
-			visitPre(ast, matches.push.bind(matches));
-			break;
+		/**
+		 * Create an error object with the supplied information.
+		 */
+		function createError(message, token, tokens, ast) {
+			return new Error(message + JSON.stringify(token) + "\n" +
+					"Remaining tokens: " + JSON.stringify(tokens, null, "  ") + "\n" +
+					"Current ast: " + JSON.stringify(ast, null, "  "));
+		}
 
-		case "identifier":
-			visitPre(ast, function (node) {
-				if (node.type === selector.value) {
-					matches.push(node);
+		/**
+		 * Walk the ECMAScript AST with a pre-order traversal. If the callback function
+		 * returns something, then that will be passed to the subtree node visits.
+		 */
+		function visitPre(ast, fn, context) {
+			var newContext = fn(ast, context);
+			context = newContext !== undefined ? newContext : context;
+
+			var key;
+			for (key in ast) {
+				if (ast[key] && ast[key].forEach) {
+					ast[key].forEach(function (node) {
+						visitPre(node, fn, context);
+					});
+				} else if (ast[key] && ast[key].type) {
+					visitPre(ast[key], fn, context);
 				}
-			});
-			break;
+			}
+		}
 
-		case "descendant":
-			leftMatches = match(ast, selector.left);
-			rightMatches = match(ast, selector.right);
-
-			visitPre(ast, function (node, context) {
-				if (context && rightMatches.indexOf(node) > -1) {
-					matches.push(node);
+		/**
+		 * Get the value of a property which may be multiple levels down in the object.
+		 */
+		function getPath(obj, key) {
+			var keys = key.split(".");
+			var i;
+			value = obj;
+			for (i = 0; i < keys.length; i++) {
+				if (value[keys[i]] !== undefined) {
+					value = value[keys[i]];
+				} else {
+					return undefined;
 				}
+			}
+			return value;
+		}
 
-				if (leftMatches.indexOf(node) > -1) {
-					return true;
-				}
-			});
 
-			break;
+		/**
+		 * This is the core match method. It takes the code AST and the selector AST
+		 * and returns the matching nodes of the code.
+		 */
+		function match(ast, selector) {
+			if (!selector) {
+				return [];
+			}
 
-		case "and":
-			leftMatches = match(ast, selector.left);
-			rightMatches = match(ast, selector.right);
+			var leftMatches, rightMatches, matches = [];
 
-			matches = leftMatches.filter(function (leftNode) {
-				return rightMatches.indexOf(leftNode) > -1;
-			});
-			break;
+			switch (selector.type) {
+			case "wildcard":
+				visitPre(ast, matches.push.bind(matches));
+				break;
 
-		case "attribute":
-			visitPre(ast, function (node) {
-				switch (selector.value.type) {
-				case "literal":
-					if (getPath(node, selector.name) === selector.value.value) {
+			case "identifier":
+				visitPre(ast, function (node) {
+					if (node.type === selector.value) {
+						matches.push(node);
+					}
+				});
+				break;
+
+			case "descendant":
+				leftMatches = match(ast, selector.left);
+				rightMatches = match(ast, selector.right);
+
+				visitPre(ast, function (node, context) {
+					if (context && rightMatches.indexOf(node) > -1) {
+						matches.push(node);
+					}
+
+					if (leftMatches.indexOf(node) > -1) {
+						return true;
+					}
+				});
+
+				break;
+
+			case "child":
+				leftMatches = match(ast, selector.left);
+				rightMatches = match(ast, selector.right);
+
+				visitPre(ast, function (node, context) {
+					if (context > 0 && rightMatches.indexOf(node) > -1) {
+						matches.push(node);
+					}
+
+					return leftMatches.indexOf(node) > -1 ? 1 : 0;
+				});
+
+				break;
+
+			case "sibling":
+				leftMatches = match(ast, selector.left);
+				rightMatches = match(ast, selector.right);
+
+				visitPre(ast, function (node, context) {
+					Object.keys(node).forEach(function (key) {
+						if (node[key] && node[key].forEach) {
+							var rightNode, hasLeft = hasRight = false
+							node[key].forEach(function (child) {
+								hasLeft |= leftMatches.indexOf(child) > -1;
+								if (rightMatches.indexOf(child) > -1) {
+									rightNode = child;
+									hasRight = true;
+								}
+							});
+
+							if (hasLeft && hasRight) {
+								matches.push(rightNode);
+							}
+						}
+					});
+				});
+
+				break;
+
+			case "nth-child":
+				visitPre(ast, function (node, context) {
+					var index = selector.index.value;
+					Object.keys(node).forEach(function (key) {
+						if (node[key] && node[key].forEach) {
+							var len = node[key].length;
+							if (index >= 0 && index < len) {
+								matches.push(node[key][index]);
+							} else if (index < 0 && len + index < len && len + index >= 0) {
+								matches.push(node[key][len + index]);
+							}
+						}
+					});
+				});
+
+				break;
+
+			case "adjacent":
+				leftMatches = match(ast, selector.left);
+				rightMatches = match(ast, selector.right);
+
+				visitPre(ast, function (node, context) {
+					Object.keys(node).forEach(function (key) {
+						if (node[key] && node[key].forEach) {
+							var rightNode, leftIndex = rightIndex = -1;
+							node[key].forEach(function (child, i) {
+								if (leftMatches.indexOf(child) > -1) {
+									leftIndex = i;
+								}
+								if (rightMatches.indexOf(child) > -1) {
+									rightNode = child;
+									rightIndex = i;
+								}
+							});
+
+							if (Math.abs(leftIndex - rightIndex) === 1) {
+								matches.push(rightNode);
+							}
+						}
+					});
+				});
+
+				break;
+
+			case "and":
+				leftMatches = match(ast, selector.left);
+				rightMatches = match(ast, selector.right);
+
+				matches = leftMatches.filter(function (leftNode) {
+					return rightMatches.indexOf(leftNode) > -1;
+				});
+				break;
+
+			case "attribute":
+				switch (selector.operator) {
+				case "=":
+					switch (selector.value.type) {
+					case "literal":
+						visitPre(ast, function (node) {
+							if (getPath(node, selector.name) === selector.value.value) {
+								matches.push(node);
+							}
+						});
+						break;
+					}
+					break;
+
+				case undefined:
+					if (getPath(node, selector.name) !== undefined) {
 						matches.push(node);
 					}
 					break;
 				}
-			});
-			break;
+				break;
+			}	
+
+			return matches;
 		}
 
-		return matches;
-	}
-	
-    function query(ast, selector) {
-    	return match(ast, parse(selector));
-    }
+		/**
+		 * Parse a selector string and return it's AST.
+		 */
+		function parse(selector) {
+			return processTokens(tokenize(selector));
+		}
+		
+		/**
+		 * Query the code AST using the selector string.
+		 */
+	    function query(ast, selector) {
+	    	return match(ast, parse(selector));
+	    }
 
-    this.esquery = query;
+	    query.tokenize = tokenize;
+	    query.processTokens = processTokens;
+	    query.parse = parse;
+	    query.match = match;
+	    return query;
+	}
+
+
+	if (typeof define === "function") {
+    	define(esqueryModule);
+	} else if (typeof module !== 'undefined' && module.exports) {
+    	module.exports = esqueryModule();
+	} else {
+		this.esquery = esqueryModule();
+	}
 })();
