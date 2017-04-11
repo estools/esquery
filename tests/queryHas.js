d06d88bbebe66a4e9682a4acc44df5c7df5c142b
Stephen Belanger <admin@stephenbelanger.com>
2017-04-11 02:50:51 -0700

    Expand :has() tests

--- tests/queryHas.js
+++ tests/queryHas.js
@@ -11,6 +11,26 @@ define([
         "conditional": function () {
             var matches = esquery(conditional, 'ExpressionStatement:has([name="foo"][type="Identifier"])');
             assert.isEqual(1, matches.length);
+        },
+
+        "one of": function () {
+            var matches = esquery(conditional, 'IfStatement:has(LogicalExpression [name="foo"], LogicalExpression [name="x"])');
+            assert.isEqual(1, matches.length);
+        },
+
+        "chaining": function () {
+            var matches = esquery(conditional, 'BinaryExpression:has(Identifier[name="x"]):has(Literal[value="test"])');
+            assert.isEqual(1, matches.length);
+        },
+
+        "nesting": function () {
+            var matches = esquery(conditional, 'Program:has(IfStatement:has(Literal[value=true], Literal[value=false]))');
+            assert.isEqual(1, matches.length);
+        },
+
+        "non-matching": function () {
+            var matches = esquery(conditional, ':has([value="impossible"])');
+            assert.isEqual(0, matches.length);
         }
 
     });
