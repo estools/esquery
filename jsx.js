8a21ecfc7f9718b72ebab105ecfdcba5731f0b59
allesklarbeidir <sebastian.jerratsch@gmail.com>
2017-06-12 14:24:57 +0200

    Optional init-script for JSX-Support
    require("esquery/jsx") now loads this script which creates the module with the estraverse-fb module instead of the original one.
    This adds support for JSX-Nodes in a "plug-in" way because estraverse-fb also just extends the estraverse module and this way the most up to date estraverse module can always be used.

---
+++ jsx.js
@@ -0,0 +1,16 @@
+/* vim: set sw=4 sts=4 : */
+(function () {
+
+    var esquery = require("./esquery_init");
+    var estraverse_jsx = require("estraverse-fb");
+
+
+    if (typeof define === "function" && define.amd) {
+        define(esquery(estraverse_jsx));
+    } else if (typeof module !== 'undefined' && module.exports) {
+        module.exports = esquery(estraverse_jsx);
+    } else {
+        this.esquery = esquery(estraverse_jsx);
+    }
+
+})();
