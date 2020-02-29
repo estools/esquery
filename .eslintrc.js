d8cb11ec90c8f9544dcf15d0eef4c4f4145477cc
Brett Zamir <brettz9@yahoo.com>
2020-02-29 21:13:59 +0800

    - Linting (ESLint): Add `node_modules` and `dist` to ignore file and unignore `.eslintrc.js`;     use "js" extension for comments; enforce `semi` rule; lint `parser.js` to catch any buggy generator issues - Linting (package.json): Add recommended fields (contributors, bugs, homepage); update repository URL - Refactoring: Fix broken or redundant HTML - Maintenance: Enforce 2 sp. for JSON in `.editorconfig` (per current `package.json`) - npm: Add build scripts (leading currently to Makefile)

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true
+    },
+    extends: "eslint:recommended",
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+        // Remove after converting to ESM
+        define: "readonly"
+    },
+    overrides: [{
+      files: 'parser.js',
+      rules: {
+        // We could disable checking as auto-generated
+        'no-control-regex': 0,
+        'no-unused-vars': 0,
+        'no-useless-escape': 0,
+        semi: 0
+      }
+    }],
+    parserOptions: {
+        ecmaVersion: 2018
+    },
+    rules: {
+        semi: ["error"]
+    }
+};
